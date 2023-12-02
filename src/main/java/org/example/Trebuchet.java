bd928283eb78aed74356102abd8a8dea626b12ab
thinhmvo <thinhmvo101@gmail.com>
2023-12-01 21:49:10 -0500

    solved part one

--- src/main/java/org/example/Trebuchet.java
+++ src/main/java/org/example/Trebuchet.java
@@ -12,24 +12,18 @@ public class Trebuchet {
     try {
         File file = new File("D:\\workspace\\mike\\AdventOfCode\\src\\main\\resources\\trebuchet.txt");
         Scanner scanner = new Scanner(file);
-        // Initialize 2 pointer that point to each end of the array
-        int sum = 0;
-        // Read the file line by line
+           int sum = 0;
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             // Split the line into an array of strings
-            String[] lineArray = line.split("");
-            // Loop through the array of string
-           // If the first pointer and the second pointer is a digit, combine them to form a number
-            // If neither the first pointer nor the second pointer is a digit, move both pointers to the next element
-            // Find an element that is a digit and concatenate it with itself to form a number
+            String intString = line.replaceAll("[^0-9]", "");
+            char[] charArray = intString.toCharArray();
 
+            String wholeNumElement = "" + charArray[0] + charArray[charArray.length - 1];
+            System.out.println(wholeNumElement);
 
+            sum += Integer.parseInt(wholeNumElement);
 
-            for (int i = 0; i < lineArray.length; i++) {
-
-}
-            System.out.println(line);
         }
         System.out.println("Sum: " + sum);
         scanner.close();
